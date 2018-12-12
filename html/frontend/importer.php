52615ce3270edbe05206a9594c0bf153578ba94a
James Bithell <james@bithell.com>
2018-12-12 16:44:42 +0000

    Write an importer for articles

---
+++ html/frontend/importer.php
@@ -0,0 +1,81 @@
+<?php
+require_once __DIR__ . '/common/head.php';
+die("");
+for ($x = 0; $x <= 14; $x++) {
+    $data = json_decode(file_get_contents("data/nouseFULL" . ($x < 10 ? "0" : "") . $x . ".json"), true);
+
+    foreach ($data["posts"] as $post) {
+        if ($post["post_type"] != "post") continue;
+        if ($post["post_parent"] != "0") {
+            var_dump($post);
+            continue;
+        }
+        $postData = [
+            "articles_id" => $post["post_id"],
+            "articles_authors" => "",
+            "articles_published" => ($post["post_date_gmt"] == "0000-00-00 00:00:00" ? $post["post_date"] : $post["post_date_gmt"]),
+            "articles_updated" => ($post["post_date_gmt"] == "0000-00-00 00:00:00" ? $post["post_date"] : $post["post_date_gmt"]),
+            "articles_showInLists" => ($post['status'] == "publish" ? 1 : 0),
+            "articles_showInSearch" => ($post['status'] == "publish" ? 1 : 0),
+            "articles_showInAdmin" => 1,
+            "articles_type" => 1,
+            "articles_slug" => $post["link"],
+            "articles_extraMetadata" => json_encode($post["postmeta"]),
+            "articles_lifetimeViews" => (isset($post["postmeta"]["views"]) ? $post["postmeta"]["views"] : null)
+        ];
+        if (isset($post['_thumbnail_id'])) $postData["articles_thumbnail"] = $post["postmeta"]["_thumbnail_id"];
+        elseif (isset($post['postmeta']["_articlephoto_url"])) {
+            $postData["articles_thumbnail"] = $post["postmeta"]["_articlephoto_url"];
+        }
+        $postCategories = [];
+        foreach ($post['categories'] as $category) {
+            $DBLIB->where("categories_name", $category);
+            $category = $DBLIB->getone("categories", ["categories_id"]);
+            if ($category) array_push($postCategories, $category["categories_id"]);
+        }
+        $postData["articles_categories"] = implode(",", $postCategories);
+
+        $postID = $DBLIB->insert("articles", $postData);
+        if (!$postID) {
+            var_dump($post);
+            echo $DBLIB->getLastError() . "\n";
+        }
+
+        $postDraftData = [
+            "articles_id" => $postID,
+            "articlesDrafts_timestamp" => ($post["post_date_gmt"] == "0000-00-00 00:00:00" ? $post["post_date"] : $post["post_date_gmt"]),
+            "articlesDrafts_headline" => ($post["title"] != null ? $post['title'] : $post["link"]),
+            "articlesDrafts_byline" => $post["excerpt"],
+            "articlesDrafts_text" => $post["content"],
+        ];
+        if (isset($post['postmeta']["_articlephoto_caption"]) and $post['postmeta']["_articlephoto_caption"] != null) {
+            $postDraftData["articlesDrafts_byline"] .= " (Thumbnail credit " . $post['postmeta']["_articlephoto_caption"] . ")";
+        }
+        if (!$DBLIB->insert("articlesDrafts", $postDraftData)) {
+            var_dump($post);
+            echo $DBLIB->getLastError() . "\n";
+        }
+
+        foreach ($post['comments'] as $comment) {
+            $commentData = [
+                "comments_id" => $comment['id'],
+                "comments_authorName" => $comment["author"],
+                "comments_authorEmail" => $comment['author_email'],
+                "comments_authorURL" => $comment["author_url"],
+                "comments_authorIP" => $comment["author_ip"],
+                "comments_created" => ($comment["date_gmt"] == "0000-00-00 00:00:00" ? $comment["date"] : $comment["date_gmt"]),
+                "comments_text" => $comment["content"],
+                "comments_show" => ($comment["approved"] == 1 ? 1 : 0),
+                "users_userid" => $comment["user_id"],
+                "articles_id" => $postID,
+            ];
+            if (!$DBLIB->insert("comments", $commentData)) {
+                echo $DBLIB->getLastError() . "\n";
+                var_dump($comment);
+            }
+        }
+    }
+}
+?>
+
+
