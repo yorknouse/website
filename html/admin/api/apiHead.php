5c3f1ed45f4002c1f40270fb2775624351f53273
James Bithell <james@bithell.com>
2019-01-10 17:10:50 +0000

    Try and correct header send order issue

--- html/admin/api/apiHead.php
+++ html/admin/api/apiHead.php
@@ -1,9 +1,9 @@
 <?php
-require_once __DIR__ . '/../common/head.php';
 header('Content-type: application/json');
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+require_once __DIR__ . '/../common/head.php';
 function finish($result = false, $error = ["code" => null, "message"=> null], $response = []) {
     $dataReturn = ["result" => $result];
     if ($error) $dataReturn["error"] = $error;
