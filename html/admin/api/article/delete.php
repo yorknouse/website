c75b9e96a3a2e4b343db1999f5e225b8df839742
James Bithell <james@bithell.com>
2019-03-27 14:35:32 +0000

    Delete from apple news if deleted

--- html/admin/api/article/delete.php
+++ html/admin/api/article/delete.php
@@ -7,12 +7,14 @@ if ((!$AUTH->permissionCheck(33) and !isset($_GET['articleid'])) or !is_numeric(
 $bCMS->auditLog("DELETE", "articles", $bCMS->sanitizeString($_GET['articleid']), $AUTH->data['users_userid']);
 
 $DBLIB->where ('articles_id', $bCMS->sanitizeString($_GET['articleid']));
-$article = $DBLIB->getOne("articles", ["articles_id", "articles_published", "articles_slug","articles_categories"]);
+$article = $DBLIB->getOne("articles", ["articles_id", "articles_published", "articles_slug","articles_categories", "articles_appleNewsID"]);
 
 $bCMS->cacheClear($CONFIG['ROOTFRONTENDURL'] . "/" . date("Y/m/d", strtotime($article['articles_published'])) . "/" . $article['articles_slug']);
 foreach (explode(",", $article['articles_categories']) as $category) {
     $bCMS->cacheClearCategory($category);
 }
+
+$bCMS->deleteAppleNews($article['articles_id']); //Remove from apple news
 
 $DBLIB->where ('articles_id', $article['articles_id']);
 if ($DBLIB->update ('articles', ["articles_showInAdmin" => 0, "articles_showInSearch" => 0, "articles_showInLists" => 0])) die('1');
