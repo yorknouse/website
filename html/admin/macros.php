e45525d132bd493ae990635e06e22d070a590bda
James Bithell <james@bithell.com>
2019-06-26 13:24:24 +0100

    Begin building support for Macros to help admins with annual events

---
+++ html/admin/macros.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/common/headSecure.php';
+
+$PAGEDATA['pageConfig'] = ["TITLE" => "Macros", "BREADCRUMB" => true];
+
+if (!$AUTH->permissionCheck(43)) die("Sorry - you can't access this page");
+
+echo $TWIG->render('macros.twig', $PAGEDATA);
+?>
