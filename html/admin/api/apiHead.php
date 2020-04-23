08dbfe2a2088576084db7711372b2042cb981bbf
James Bithell <james@bithell.com>
2020-04-23 19:05:26 +0100

    Remove errors from API as they break JS

--- html/admin/api/apiHead.php
+++ html/admin/api/apiHead.php
@@ -4,6 +4,8 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 require_once __DIR__ . '/../common/head.php';
+ini_set('display_errors', 0); //Errors break the output
+
 function finish($result = false, $error = ["code" => null, "message"=> null], $response = []) {
     $dataReturn = ["result" => $result];
     if ($error) $dataReturn["error"] = $error;
