a59031e1a5db4fc9e7340d2bdaf4916d352c0ce9
James Bithell <james@bithell.com>
2020-04-29 22:47:55 +0100

    Include editions type in list

--- html/frontend/editions/index.php
+++ html/frontend/editions/index.php
@@ -8,7 +8,7 @@ $DBLIB->where("editions_deleted", 0); //ie those that can actually be shown
 $DBLIB->where("(editions_thumbnail IS NOT NULL)");
 $DBLIB->where("editions.editions_show",1);
 $DBLIB->where("editions.editions_published <= '" . date("Y-m-d H:i:s") . "'");
-$PAGEDATA['editions'] = $DBLIB->get("editions", null, ["editions_slug", "editions_published","editions_printNumber","editions_thumbnail","editions_name","editions_excerpt"]);
+$PAGEDATA['editions'] = $DBLIB->get("editions", null, ["editions_slug", "editions_published","editions_printNumber","editions_thumbnail","editions_name","editions_excerpt","editions_type"]);
 
 echo $TWIG->render('editions.twig', $PAGEDATA);
 ?>
