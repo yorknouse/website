12f43118da64d54f4ed815334daeeaf37a4ec52b
James Bithell <james@bithell.com>
2020-05-03 17:03:56 +0100

    Remove facebook likebox

--- html/frontend/common/assets/theme/osru/assets/js/custom.js
+++ html/frontend/common/assets/theme/osru/assets/js/custom.js
@@ -422,6 +422,7 @@
 // Facebook Like Box
 // -------------------------------------------------------------------------- //
         fbLikeBox: function () {
+            /*
             (function (d, s, id) {
                 var js, fjs = d.getElementsByTagName(s)[0];
                 if (d.getElementById(id))
@@ -431,6 +432,7 @@
                 js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12';
                 fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));
+            */
         },
 // -------------------------------------------------------------------------- //
 // Youtube video
