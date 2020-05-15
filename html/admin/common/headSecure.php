f96f51f893d92c9fb033e4d10b2c1da290314336
James Bithell <james@bithell.com>
2020-05-15 12:00:46 +0100

    Closes https://github.com/yorknouse/website/issues/89

--- html/admin/common/headSecure.php
+++ html/admin/common/headSecure.php
@@ -3,7 +3,6 @@ require_once __DIR__ . '/head.php';
 require_once __DIR__ . '/../common/authLib.php';
 
 if (!$GLOBALS['AUTH']->login) {
-    if ($GLOBALS['AUTH']->loginErrorMessage) die($GLOBALS['AUTH']->loginErrorMessage . '<br/><br/><a href="' . $CONFIG['ROOTBACKENDURL'] . "/login/" . '">Retry</a>');
     $_SESSION['return'] = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header("Location: " . $CONFIG['ROOTBACKENDURL'] . "/login/");
     die('<meta http-equiv="refresh" content="0; url="' . $CONFIG['ROOTBACKENDURL'] . "/login/" . '" />');
