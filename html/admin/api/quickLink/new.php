272ca666039765952b94352ed0f9861cb90322e3
James Bithell <james@bithell.com>
2020-09-13 14:44:51 +0100

    Tweak handling of & character

--- html/admin/api/quickLink/new.php
+++ html/admin/api/quickLink/new.php
@@ -9,8 +9,8 @@ $DBLIB->where ('quickLinks_deleted', 0);
 if ($DBLIB->getValue("quickLinks", "COUNT(*)") > 0) die("TAKEN");
 
 $data = [
-    "quickLinks_string" => $bCMS->sanitizeString(urldecode($_GET['string'])),
-    "quickLinks_pointsTo" => str_replace($CONFIG['ROOTFRONTENDURL'], '', $bCMS->sanitizeString(urldecode($_GET['destination']))),
+    "quickLinks_string" => str_replace('&amp;', '&', $bCMS->sanitizeString(urldecode($_GET['string']))),
+    "quickLinks_pointsTo" => str_replace('&amp;', '&', str_replace($CONFIG['ROOTFRONTENDURL'], '', $bCMS->sanitizeString(urldecode($_GET['destination'])))),
     "quickLinks_notes" => $bCMS->sanitizeString($_GET['notes']),
     "quickLinks_created" => date("Y-m-d H:i:s"),
     "users_userid" => $AUTH->data['users_userid']
