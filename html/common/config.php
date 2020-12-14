343a3171f3a2a4ecf0bea4fd74b3f3276db49662
James Bithell <james@bithell.com>
2020-12-14 17:47:55 +0000

    Move all files over to Backblaze from DigitalOcean Spaces

--- html/common/config.php
+++ html/common/config.php
@@ -34,7 +34,9 @@ $CONFIG = array(
     "ROOTFRONTENDURL" => getenv('bCMS__FRONTENDURL'), //Set on a frontend/backend basis
     "ROOTBACKENDURL" => getenv('bCMS__BACKENDURL'), //Set on a frontend/backend basis
     "PROJECT_SUPPORT_EMAIL" => getenv('bCMS__SUPPORTEMAIL'),
-    "FILESTOREURL" => getenv('bCMS__FILESTOREURL'),
+    "FILESTOREURL" => "https://bbcdn.nouse.co.uk/file/",
+    "ASSETSFILESTOREURL" => "https://bbcdn.nouse.co.uk/file/nouseSiteAssets/", //Site assets like logos etc
+    "ARCHIVEFILESTOREURL" => "https://bbcdn.nouse.co.uk/file/nouseOldImageLibrary/archive/public", //Images pre 2019
     "RECAPTCHA" => ['KEY' => getenv('bCMS__RECAPTCHA_KEY'), 'SECRET' => getenv('bCMS__RECAPTCHA_SECRET')],
     'AWS' => [
         'UPLOAD' => true,
