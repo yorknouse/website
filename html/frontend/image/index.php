ae32d5d71fe2f69d09db8cafabc10d04826dfe87
James Bithell <james@bithell.com>
2021-01-12 14:36:54 +0000

    Correct bug in image upload not redirecting correctly

--- html/frontend/image/index.php
+++ html/frontend/image/index.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../common/head.php';
  *      f (required) - the file id as specified in the database
  *      s (filesize) - false to get the original - available is "tiny" (50px) "small" (100px) "medium" (500px) "large" (1000px)
  *      d (optional, default false) - should a download be forced or should it be displayed in the browser? (if set it will download)
- *      r (optional, default false) - should the url be returned by the script as plain text or a redirect triggered? (if set it will return it)
+ *      r (optional, default false) - should the url be returned by the script as plain text or a redirect triggered? (if set it will redirect)
  *      e (optional, default 1 minute) - when should the link expire? Must be a string describing how long in words basically. If this file type has security features then it will default to 1 minute.
  */
 
