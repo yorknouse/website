60c21d73c34877249d01b68a465ef9bba869051c
James Bithell <james@bithell.com>
2021-02-16 19:23:19 +0000

    Allow articles to be searched for in editions, even if they're not published

--- html/admin/api/featured/articleSearchEdition.php
+++ html/admin/api/featured/articleSearchEdition.php
@@ -11,8 +11,7 @@ if (isset($_POST['articleid'])) {
     if (strlen($term) > 0) $DBLIB->where("(articlesDrafts.articlesDrafts_headline LIKE '%" . $bCMS->sanitizeString($term) . "%')");
 }
 $DBLIB->orderBy("articles_published", "DESC");
-$DBLIB->where("articles_showInLists", 1);
-$DBLIB->where("articles_published <= '" . date("Y-m-d H:i:s") . "'");
+$DBLIB->where("articles_showInAdmin", 1);
 $DBLIB->where("editions_id", $_GET['editionid']);
 $DBLIB->join("articlesDrafts", "articles.articles_id=articlesDrafts.articles_id", "LEFT");
 $DBLIB->where("articlesDrafts_id = (SELECT articlesDrafts_id FROM articlesDrafts WHERE articlesDrafts.articles_id=articles.articles_id ORDER BY articlesDrafts_timestamp DESC LIMIT 1)");
