d2be5942189c3d1fcded6c2de6238aa50c0d291d
Omar Omar <omarhatem5555@gmail.com>
2023-01-25 23:41:15 +0000

    testing github pages deployment for astro

--- readers-frontend/astro.config.mjs
+++ readers-frontend/astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://yorknouse.github.io',
+  base: '/readers-frontend',
 });
