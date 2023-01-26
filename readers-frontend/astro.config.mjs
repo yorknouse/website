8dea7480c12c8708ccbe892a60d10e42d9d42999
Omar Omar <omarhatem5555@gmail.com>
2023-01-26 11:21:53 +0000

    changing base to repo name

--- readers-frontend/astro.config.mjs
+++ readers-frontend/astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://yorknouse.github.io',
-  base: '/readers-frontend'
+  base: '/website'
 });
