404a478f6e8b4ff593f415943034d4e36b34d205
Omar Omar <omarhatem5555@gmail.com>
2023-01-26 11:19:40 +0000

    adding back the base

--- readers-frontend/astro.config.mjs
+++ readers-frontend/astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://yorknouse.github.io'
+  site: 'https://yorknouse.github.io',
+  base: '/readers-frontend'
 });
