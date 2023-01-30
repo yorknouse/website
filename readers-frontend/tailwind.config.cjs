6f50c6d1a4aacf063b1c2c3f351304ddeaf50b61
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-01-30 16:25:21 +0000

    Navbar: Tweak logo and font
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- readers-frontend/tailwind.config.cjs
+++ readers-frontend/tailwind.config.cjs
@@ -6,6 +6,16 @@ module.exports = {
       colors: {
         whiteish: "#f5efeb",
       },
+      spacing: {
+        81: "21.563rem/* 345px */",
+        15: "3.875rem/* 62px */",
+      },
+      backgroundSize: {
+        fill: "100% 100%",
+      },
+      backgroundImage: {
+        logo: "url('https://bbcdn.nouse.co.uk/file/nouseSiteAssets/logo/nouse-logo-full3.svg')",
+      },
     },
   },
   plugins: [],
