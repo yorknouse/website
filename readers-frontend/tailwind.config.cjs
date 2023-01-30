5cb9c876f012c6f076215251b5de06961931f470
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-01-30 17:34:36 +0000

    Navbar: Initial Muse conf
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- readers-frontend/tailwind.config.cjs
+++ readers-frontend/tailwind.config.cjs
@@ -7,14 +7,19 @@ module.exports = {
         whiteish: "#f5efeb",
       },
       spacing: {
-        81: "21.563rem/* 345px */",
         15: "3.875rem/* 62px */",
+        17: "4.438rem/* 71px */",
+        71: "17.688rem/* 283px */",
+        81: "21.563rem/* 345px */",
       },
       backgroundSize: {
         fill: "100% 100%",
       },
       backgroundImage: {
-        logo: "url('https://bbcdn.nouse.co.uk/file/nouseSiteAssets/logo/nouse-logo-full3.svg')",
+        "nouse-logo":
+          "url('https://bbcdn.nouse.co.uk/file/nouseSiteAssets/logo/nouse-logo-full3.svg')",
+        "muse-logo":
+          "url('https://bbcdn.nouse.co.uk/file/nouseSiteAssets/logo/MUSE%20Logo%20White%20small.png')",
       },
     },
   },
