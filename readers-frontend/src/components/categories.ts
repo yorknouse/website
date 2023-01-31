b355f6780b5dbefc0ff7cb626a689736264c10ef
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-01-31 10:12:19 +0000

    Navbar: Move logic to separate file
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

---
+++ readers-frontend/src/components/categories.ts
@@ -0,0 +1,94 @@
+import type { categories } from "@prisma/client";
+import prisma from "../prisma";
+
+const getMenuCategories = async (
+  style: "nouse" | "muse"
+): Promise<categories[]> => {
+  let menuCategories: categories[] = await prisma.categories.findMany({
+    where: {
+      categories_showMenu: true,
+      categories_nestUnder: null,
+    },
+  });
+
+  // Add home as first item - possibly need to adjust values in the future
+  if (style === "nouse") {
+    menuCategories.unshift({
+      categories_name: "home",
+      categories_id: 0,
+      categories_showHome: true,
+      categories_displayName: "Home",
+      categories_showMenu: true,
+      categories_showPublic: true,
+      categories_showAdmin: true,
+      categories_featured: null,
+      categories_order: null,
+      categories_nestUnder: null,
+      categories_showSub: false,
+      categories_facebook: null,
+      categories_twitter: null,
+      categories_instagram: null,
+      categories_backgroundColor: null,
+      categories_backgroundColorContrast: null,
+      categories_customTheme: null,
+      categories_socialMediaOverlay: null,
+    });
+
+    // Muse to last
+    menuCategories = menuCategories.sort((a, b) => {
+      if (b.categories_name === "muse") return -1;
+      if (a.categories_name === "muse") return 1;
+
+      return 0;
+    });
+  } else {
+    // Muse to first
+    menuCategories = menuCategories.sort((a, b) => {
+      if (b.categories_name === "muse") return 1;
+      if (a.categories_name === "muse") return -1;
+
+      return 0;
+    });
+
+    // Link to Nouse home
+    menuCategories.push({
+      categories_name: "nouse",
+      categories_id: 0,
+      categories_showHome: true,
+      categories_displayName: "Nouse",
+      categories_showMenu: true,
+      categories_showPublic: true,
+      categories_showAdmin: true,
+      categories_featured: null,
+      categories_order: null,
+      categories_nestUnder: null,
+      categories_showSub: false,
+      categories_facebook: null,
+      categories_twitter: null,
+      categories_instagram: null,
+      categories_backgroundColor: null,
+      categories_backgroundColorContrast: null,
+      categories_customTheme: null,
+      categories_socialMediaOverlay: null,
+    });
+  }
+
+  return menuCategories;
+};
+
+export default getMenuCategories;
+
+// Construct nested items map - saved for later
+// let nestedItems = menuCategories.reduce((accumulator, category, _) => {
+//   const parent = category.categories_nestUnder;
+//   if (parent !== null) {
+//     let subCategories = accumulator.get(parent);
+
+//     if (!subCategories) subCategories = [];
+
+//     subCategories.push(category);
+
+//     accumulator.set(parent, subCategories);
+//   }
+//   return accumulator;
+// }, new Map<number, categories[]>());
