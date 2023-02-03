39ccd175178abbb9c878dbc85a82170fb052a2f5
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-02-03 14:28:35 +0000

    Move categories to utils folder
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- readers-frontend/src/components/utils/categories.ts
+++ readers-frontend/src/components/utils/categories.ts
@@ -1,5 +1,5 @@
 import type { categories } from "@prisma/client";
-import prisma from "../prisma";
+import prisma from "../../prisma";
 
 const getMenuCategories = async (
   style: "nouse" | "muse"
