8a41427a6fe624ffe22b8ef84d1b1cd7b7fc4404
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-02-03 16:21:04 +0000

    Cleanup and better header sizing
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- readers-frontend/tailwind.config.cjs
+++ readers-frontend/tailwind.config.cjs
@@ -6,23 +6,6 @@ module.exports = {
       colors: {
         whiteish: "#f5efeb",
       },
-      spacing: {
-        15: "3.875rem/* 62px */",
-        17: "4.438rem/* 71px */",
-        18: "4.75rem/* 76px */",
-        63: "15.625rem/* 250px */",
-        71: "17.688rem/* 283px */",
-        81: "21.563rem/* 345px */",
-      },
-      backgroundSize: {
-        fill: "100% 100%",
-      },
-      backgroundImage: {
-        "nouse-logo":
-          "url('https://bbcdn.nouse.co.uk/file/nouseSiteAssets/logo/nouse-logo-full3.svg')",
-        "muse-logo":
-          "url('https://bbcdn.nouse.co.uk/file/nouseSiteAssets/logo/MUSE%20Logo%20White%20small.png')",
-      },
     },
   },
   plugins: [],
