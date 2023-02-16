8a4d637711466f7ff52b3007c6a075fd1bad10bb
Omar Omar <omarhatem5555@gmail.com>
2023-02-16 09:54:09 +0000

    setting connection charset for prisma and adding portrait image designs

--- readers-frontend/src/prisma.ts
+++ readers-frontend/src/prisma.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
+// DB is in latin1, so we need to set the charset
+await prisma.$executeRawUnsafe('SET NAMES latin1');
 export default prisma;
