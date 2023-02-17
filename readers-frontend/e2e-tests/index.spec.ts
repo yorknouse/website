06d66f71f9a737a3137fc390980e7372f4909d1f
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-02-17 17:48:44 +0000

    BannerEdition tests
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- readers-frontend/e2e-tests/index.spec.ts
+++ readers-frontend/e2e-tests/index.spec.ts
@@ -18,3 +18,16 @@ test("navbar has correct number of items and correct image", async ({
   //   "https://bbcdn.nouse.co.uk/file/nouseSiteAssets/logo/nouse-logo-print.svg"
   // );
 });
+
+test("banner and edition display correct information", async ({ page }) => {
+  const join = page.locator("[id='join_nouse']");
+  await expect(join.locator("img")).toHaveAttribute(
+    "src",
+    "https://bbcdn.nouse.co.uk/file/nouseSiteAssets/headerImages/join_nouse.png"
+  );
+
+  // const mockEditionURL = "TODO";
+  const edition = page.locator("[id='edition']");
+  await expect(edition.locator("div > p")).toHaveCount(2);
+  // await expect(edition.locator("img")).toHaveAttribute("src", mockEditionURL);
+});
