4b9a7f4bd4beea7eb8d5fd308fd1c8f6c3f4db33
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-04-06 18:09:57 +0200

    Also search by author and published date
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- html/frontend/api/searchSuggestions.php
+++ html/frontend/api/searchSuggestions.php
@@ -23,15 +23,32 @@ if (strlen($term) < 1) {
     finish(true, null, null);
 }
 
+// Create generic term
+$term = '%' . $term . '%';
+
+// Get matching authors
+$DBLIB->where("CONCAT(TRIM(users_name1), ' ', TRIM(users_name2))", $term, "LIKE");
+$matchingAuthorsIds = $DBLIB->get("users", null, ["users.users_userid"]);
+if (is_array(($matchingAuthorsIds)) && (count($matchingAuthorsIds) > 0)) {
+    foreach ($matchingAuthorsIds as $i => $author) {
+        $matchingAuthorsIds[$i] = $author["users_userid"];
+    }
+} else {
+    $matchingAuthorsIds = array(-1);
+}
+$matchingAuthorsIds = "(" . implode(",", $matchingAuthorsIds) . ")";
+
 $DBLIB->where(
-    "(articlesDrafts.articlesDrafts_excerpt LIKE '%" .
-    $term .
-    "%' OR articlesDrafts.articlesDrafts_headline LIKE '%" .
-    $term . "%')"
+    "(articlesDrafts.articlesDrafts_excerpt LIKE ? OR articlesDrafts.articlesDrafts_headline LIKE ? OR articles_published LIKE ? OR articles_authors IN " . $matchingAuthorsIds . ")",
+    array($term, $term, $term)
 );
 $DBLIB->orderBy("articles_published", "DESC");
 $DBLIB->where("articles_showInSearch", 1);
-$DBLIB->where("articles_published <= '" . date("Y-m-d H:i:s") . "'");
+$DBLIB->where("articles_published <= ?", array(date("Y-m-d H:i:s")));
+// Below only matches if author is 1 and exact match.
+// It should be fine for now as frontend does not support displaying multiple authors regardless
+$DBLIB->join("users", "users.users_userid=articles.articles_authors");
+$DBLIB->join("categories", "articles.articles_categories=categories.categories_id");
 $DBLIB->join("articlesDrafts", "articles.articles_id=articlesDrafts.articles_id", "LEFT");
 $DBLIB->where(
     "articlesDrafts_id =
@@ -40,8 +57,6 @@ $DBLIB->where(
     WHERE articlesDrafts.articles_id=articles.articles_id
     ORDER BY articlesDrafts_timestamp DESC LIMIT 1)"
 );
-$DBLIB->join("users", "articlesDrafts.articlesDrafts_userid=users.users_userid");
-$DBLIB->join("categories", "articles.articles_categories=categories.categories_id");
 $articles = $DBLIB->get(
     "articles",
     null,
