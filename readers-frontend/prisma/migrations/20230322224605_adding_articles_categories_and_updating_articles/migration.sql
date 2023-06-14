78b08b9caca6e4850f0bbf572e1070d8ecfe276e
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-06-14 21:26:40 +0100

    Update migrations syntax for prod
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- readers-frontend/prisma/migrations/20230322224605_adding_articles_categories_and_updating_articles/migration.sql
+++ readers-frontend/prisma/migrations/20230322224605_adding_articles_categories_and_updating_articles/migration.sql
@@ -13,13 +13,15 @@ ALTER TABLE `articlesCategories` ADD CONSTRAINT `articlesCategories_articles_id_
 ALTER TABLE `articlesCategories` ADD CONSTRAINT `articlesCategories_categories_id_fkey` FOREIGN KEY (`categories_id`) REFERENCES `categories`(`categories_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- Migrating categories.articles_categories to articlesCategories
+DELIMITER //
 CREATE PROCEDURE IF NOT EXISTS UPDATECATEGORIES()
 BEGIN
 INSERT INTO articlesCategories (articles_id, categories_id)
 SELECT articles.articles_id, categories.categories_id
 FROM articles
 JOIN categories ON FIND_IN_SET(categories.categories_id, articles.articles_categories) > 0;
-END;
+END//
+DELIMITER ;
 
 CALL UPDATECATEGORIES();
 
