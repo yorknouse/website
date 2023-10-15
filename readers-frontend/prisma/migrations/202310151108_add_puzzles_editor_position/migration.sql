2ff7400a2b5af5e68f00ef6812b4aaa150679539
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-10-15 11:11:01 +0100

    Migration to add Puzzles Editor position
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

---
+++ readers-frontend/prisma/migrations/202310151108_add_puzzles_editor_position/migration.sql
@@ -0,0 +1,2 @@
+-- Insert new position
+INSERT INTO positions (`positions_displayName`, `positions_positionsGroups`, `positions_rank`, `positions_teamPageGroup`) VALUES ('Puzzles Editor', 3, 53, 1);
