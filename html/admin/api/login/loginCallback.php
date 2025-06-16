0a27cbea99408629a4fa25155b7fb66698ff50be
Liam Burnand <liam.burnand@ystv.co.uk>
2025-06-16 04:28:04 +0100

    Inverting would be useful

--- html/admin/api/login/loginCallback.php
+++ html/admin/api/login/loginCallback.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../apiHead.php';
 header("Content-Type: text/json");
 
 $auth = $GLOBALS['AUTH']->login($_POST['idtoken']);
-if ($auth["result"])
+if (!$auth["result"])
     finish(false, ["message"=>$auth['errorMessage']]);
 
 finish(true,null,["return" => $auth['url']]);
