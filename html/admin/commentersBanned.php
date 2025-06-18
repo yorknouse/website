96a34f2136e81e76588e5f6087fde82c9958774b
Liam Burnand <liam.burnand@ystv.co.uk>
2025-06-18 16:21:03 +0100

    Fixing more spacing

--- html/admin/commentersBanned.php
+++ html/admin/commentersBanned.php
@@ -9,7 +9,7 @@ if (!$AUTH->permissionCheck(53)) die("Sorry - you can't access this page");
 $DBLIB->orderBy("COUNT(*)", "DESC");
 $DBLIB->orderBy("comments_authorEmail", "ASC");
 $DBLIB->where("comments_approved", 4); //ie those that can actually be shown
-$DBLIB->groupBy ("comments_authorEmail");
+$DBLIB->groupBy("comments_authorEmail");
 $PAGEDATA['list'] = $DBLIB->get("comments",null, ["COUNT(*)","comments_authorEmail"]);
 
 echo $TWIG->render('commentersBanned.twig', $PAGEDATA);
--- html/admin/index.php
+++ html/admin/index.php
@@ -12,15 +12,15 @@ $PAGEDATA['MOSTACTIVEUSERS'] = [];
 $DBLIB->where("(auditLog.users_userid IS NOT NULL)");
 $DBLIB->where("(auditLog.auditLog_timestamp >= curdate() - INTERVAL DAYOFWEEK(curdate())+6 DAY)");
 $DBLIB->join("users","auditLog.users_userid = users.users_userid","LEFT");
-$DBLIB->groupBy ("auditLog.users_userid");
-$DBLIB->orderBy ("counter","DESC");
+$DBLIB->groupBy("auditLog.users_userid");
+$DBLIB->orderBy("counter","DESC");
 $PAGEDATA['MOSTACTIVEUSERS']['WEEK'] = $DBLIB->get("auditLog",5,["users.users_name1", "users.users_name2", "auditLog.users_userid", "COUNT(*) AS counter"]);
 
 
 //My recent edits
 /*$DBLIB->orderBy("articlesDrafts.articlesDrafts_timestamp", "DESC");
 $DBLIB->where("articlesDrafts.articlesDrafts_userid",$AUTH->data['users_userid']);
-$DBLIB->groupBy ("articlesDrafts.articles_id");
+$DBLIB->groupBy("articlesDrafts.articles_id");
 $DBLIB->where("articles.articles_showInAdmin", 1);
 $DBLIB->join("articles","articles.articles_id=articlesDrafts.articles_id","LEFT");
 $PAGEDATA['recentlyEditedArticles'] = $DBLIB->get("articlesDrafts",10,["articlesDrafts.articles_id","articlesDrafts.articlesDrafts_headline"]);*/
