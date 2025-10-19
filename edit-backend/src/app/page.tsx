3eb87764fe89aca1460262a6b6a66f2b6c5a71c4
Liam Burnand <liam.burnand@ystv.co.uk>
2025-10-19 14:44:28 +0100

    Showing data from login

--- edit-backend/src/app/page.tsx
+++ edit-backend/src/app/page.tsx
@@ -1,11 +1,19 @@
 import Sidebar from "@/components/Navbar";
 import type { Metadata } from "next";
+import { useSession } from "next-auth/react";
+import {redirect} from "next/navigation";
 
 export const metadata: Metadata = {
     title: "Home",
 };
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (!session) {
+      redirect("/auth/signin");
+  }
+
   return (
       <div className="lg:flex min-h-screen bg-gray-200 text-gray-900">
           <Sidebar />
@@ -19,6 +27,10 @@ export default function Home() {
                   <p>
                       If you have any issues using the website then please reach out to <code>#tech</code> on Slack for guidance or help or to report an issue
                   </p>
+              </div>
+              <div className="flex items-center gap-2">
+                  <span>{session.user?.name}</span>
+                  <span>{session.user?.email}</span>
               </div>
           </main>
       </div>
