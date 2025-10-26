43cd887857d9e18419bb252e9a2925b7c3f6d3b5
Liam Burnand <liam.burnand@ystv.co.uk>
2025-10-26 13:46:04 +0000

    Moved into the function, that is why it was broken

--- edit-backend/src/app/articles/page.tsx
+++ edit-backend/src/app/articles/page.tsx
@@ -1,18 +1,18 @@
 import prisma from "@/lib/prisma";
 import type { Metadata } from "next";
 
-const articles = await prisma.articles.findMany({
-  include: {
-    articlesDrafts: true,
-  },
-  take: 20,
-});
-
 export const metadata: Metadata = {
   title: "Articles",
 };
 
-export default function Articles() {
+export default async function Articles() {
+  const articles = await prisma.articles.findMany({
+    include: {
+      articlesDrafts: true,
+    },
+    take: 20,
+  });
+
   return (
     <div className="lg:flex min-h-screen bg-gray-200 text-gray-900">
       <div className={"flex flex-col"}>
