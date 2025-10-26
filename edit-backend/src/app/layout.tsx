4948729fd3a3c4ca6f66207bc8b35f10acf5a8b8
Liam Burnand <liam.burnand@ystv.co.uk>
2025-10-26 08:31:50 +0000

    Moving main and sidebar to layout

--- edit-backend/src/app/layout.tsx
+++ edit-backend/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../css/globals.css";
 import React from "react";
 import Providers from "./providers";
+import Sidebar from "@/components/Navbar";
+import { IsLoggedIn } from "@/lib/auth";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,22 +18,42 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: {
-      default: "Nouse Backend",
-      template: "Nouse Backend | %s",
+    template: "Nouse Backend | %s",
+    default: "Nouse Backend",
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isLoggedIn = await IsLoggedIn();
+  if (!isLoggedIn) {
+    return (
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Providers>{children}</Providers>
+        </body>
+      </html>
+    );
+  }
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <Providers>{children}</Providers>
+        <Providers>
+          <div className="lg:flex min-h-screen bg-gray-200 text-gray-900">
+            <Sidebar />
+            <main className="flg:lex-1 p-6 transition-all duration-300">
+              {children}
+            </main>
+          </div>
+        </Providers>
       </body>
     </html>
   );
--- edit-backend/src/app/page.tsx
+++ edit-backend/src/app/page.tsx
@@ -1,28 +1,32 @@
-import Sidebar from "@/components/Navbar";
 import type { Metadata } from "next";
 import UserData from "@/components/UserData";
 
 export const metadata: Metadata = {
-    title: "Home",
+  title: "Home",
 };
 
 export default function Home() {
   return (
-      <div className="lg:flex min-h-screen bg-gray-200 text-gray-900">
-          <Sidebar />
-          <main className="flg:lex-1 p-6 transition-all duration-300">
-              <div className={"lg:flex-2 shadow-md rounded-lg p-4 min-w-64 max-w-128 bg-gray-50 border-t-emerald-400 border-t-2"}>
-                  <h1 className={"text-2xl font-semibold mb-4 text-center"}>Welcome to the Nouse Backend</h1>
-                  <p>
-                      Welcome to the Nouse Backend where you will be able to administer articles, editions, committee and more...
-                  </p>
-                  <br/>
-                  <p>
-                      If you have any issues using the website then please reach out to <code>#tech</code> on Slack for guidance or help or to report an issue
-                  </p>
-              </div>
-              <UserData />
-          </main>
+    <div>
+      <div
+        className={
+          "lg:flex-2 shadow-md rounded-lg p-4 min-w-64 max-w-128 bg-gray-50 border-t-emerald-400 border-t-2"
+        }
+      >
+        <h1 className={"text-2xl font-semibold mb-4 text-center"}>
+          Welcome to the Nouse Backend
+        </h1>
+        <p>
+          Welcome to the Nouse Backend where you will be able to administer
+          articles, editions, committee and more...
+        </p>
+        <br />
+        <p>
+          If you have any issues using the website then please reach out to{" "}
+          <code>#tech</code> on Slack for guidance or help or to report an issue
+        </p>
       </div>
+      <UserData />
+    </div>
   );
 }
