10c330be97fab115827e396561256800b026c65b
Liam Burnand <liam.burnand@ystv.co.uk>
2025-10-26 08:29:10 +0000

    Initial page for user articles

---
+++ edit-backend/src/app/articles/user/[userId]/page.tsx
@@ -0,0 +1,41 @@
+import { z } from "zod";
+import type { Metadata } from "next";
+import prisma from "@/lib/prisma";
+
+export const metadata: Metadata = {
+  title: "User articles",
+};
+
+export default async function UserArticles({
+  params,
+}: {
+  params: Promise<{ userId: string }>;
+}) {
+  const resolvedParams = await params;
+  const userIDParse = z
+    .preprocess((val) => (val ? val : undefined), z.coerce.number())
+    .safeParse(resolvedParams.userId);
+
+  if (!userIDParse.success) {
+    return <>Invalid User ID</>;
+  }
+
+  const user = await prisma.users.findFirst({
+    where: {
+      users_userid: userIDParse.data,
+    },
+  });
+
+  if (!user) {
+    return <>Invalid User ID</>;
+  }
+
+  return (
+    <div>
+      <p>{user.users_userid}</p>
+      <p>
+        {user.users_name1} {user.users_name2}
+      </p>
+    </div>
+  );
+}
