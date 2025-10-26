1f2acccade15c0f4fec2dd9c2a29b42cbb904425
Liam Burnand <liam.burnand@ystv.co.uk>
2025-10-26 08:30:36 +0000

    Still used for testing

--- edit-backend/src/components/UserData.tsx
+++ edit-backend/src/components/UserData.tsx
@@ -2,22 +2,35 @@
 
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 
 export default function UserData() {
-    const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
 
-    if (status === "loading") {
-        return <div><span>Loading...</span></div>
-    }
-
-    if (!session) {
-        redirect("/auth/signin");
-    }
-
+  if (status === "loading") {
     return (
-        <div className="flex items-center gap-2">
-            <span>{session.user?.name}</span>
-            <span>{session.user?.email}</span>
-        </div>
-    )
+      <div>
+        <span>Loading...</span>
+      </div>
+    );
+  }
+
+  if (!session) {
+    redirect("/auth/signin");
+  }
+
+  return (
+    <div className="flex items-center gap-2">
+      <span>{session.user?.internalId}</span>
+      <span>{session.user?.name}</span>
+      <span>{session.user?.email}</span>
+      <Image
+        src={session.user?.image!}
+        alt={"User profile image"}
+        width={32}
+        height={32}
+        className={"rounded-lg"}
+      />
+    </div>
+  );
 }
