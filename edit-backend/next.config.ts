1b1781e019d84de6e1f500d5f8b989a31a6b69ad
Liam Burnand <liam.burnand@ystv.co.uk>
2025-10-26 08:26:26 +0000

    Adding custom image urls

--- edit-backend/next.config.ts
+++ edit-backend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://lh3.googleusercontent.com/a/*"),
+      new URL("https://bbcdn.nouse.co.uk/*"),
+    ],
+  },
 };
 
 export default nextConfig;
