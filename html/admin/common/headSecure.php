54088c8cdf59372e8c03d31c2a4fea03925de2a8
Liam Burnand <liam.burnand@ystv.co.uk>
2025-10-27 21:14:58 +0000

    Rewriting to go to the index.php page of login

--- html/admin/common/headSecure.php
+++ html/admin/common/headSecure.php
@@ -5,8 +5,8 @@ require_once __DIR__ . '/../common/authLib.php';
 
 if (!$GLOBALS['AUTH']->login) {
     $_SESSION['return'] = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header("Location: " . $CONFIG['ROOTBACKENDURL'] . "/login/");
-    die('<meta http-equiv="refresh" content="0; url="' . $CONFIG['ROOTBACKENDURL'] . "/login/" . '" />');
+    header("Location: " . $CONFIG['ROOTBACKENDURL'] . "/login/index.php");
+    die('<meta http-equiv="refresh" content="0; url="' . $CONFIG['ROOTBACKENDURL'] . "/login/index.php" . '" />');
 }
 
 $PAGEDATA['USERDATA'] = $GLOBALS['AUTH']->data;
