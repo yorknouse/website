360996eec070f73c6c1b02e85ec072411be29a01
Liam Burnand <liam.burnand@ystv.co.uk>
2025-10-31 23:35:43 +0000

    Prettified I suppose

--- edit-backend/public/oldAssets/js/libs/jquery.csvtotable/jquery.csvToTable.js
+++ edit-backend/public/oldAssets/js/libs/jquery.csvtotable/jquery.csvToTable.js
@@ -55,96 +55,144 @@ HEAVILY MODIFIED BY JAMES BITHELL - OCTOBER 2015 TO SUIT OUR PURPOSES!
  *
  */
 
+(function ($) {
+  /**
+   *
+   * CSV Parser credit goes to Brian Huisman, from his blog entry entitled "CSV String to Array in JavaScript":
+   * http://www.greywyvern.com/?post=258
+   *
+   */
+  String.prototype.splitCSV = function (sep) {
+    for (
+      var thisCSV = this.split((sep = sep || ",")), x = thisCSV.length - 1, tl;
+      x >= 0;
+      x--
+    ) {
+      if (
+        thisCSV[x].replace(/"\s+$/, '"').charAt(thisCSV[x].length - 1) == '"'
+      ) {
+        if (
+          (tl = thisCSV[x].replace(/^\s+"/, '"')).length > 1 &&
+          tl.charAt(0) == '"'
+        ) {
+          thisCSV[x] = thisCSV[x]
+            .replace(/^\s*"|"\s*$/g, "")
+            .replace(/""/g, '"');
+        } else if (x) {
+          thisCSV.splice(x - 1, 2, [thisCSV[x - 1], thisCSV[x]].join(sep));
+        } else thisCSV = thisCSV.shift().split(sep).concat(thisCSV);
+      } else thisCSV[x].replace(/""/g, '"');
+    }
+    return thisCSV;
+  };
 
- (function($){
-
-	/**
-	*
-	* CSV Parser credit goes to Brian Huisman, from his blog entry entitled "CSV String to Array in JavaScript":
-	* http://www.greywyvern.com/?post=258
-	*
-	*/
-	String.prototype.splitCSV = function(sep) {
-		for (var thisCSV = this.split(sep = sep || ","), x = thisCSV.length - 1, tl; x >= 0; x--) {
-			if (thisCSV[x].replace(/"\s+$/, '"').charAt(thisCSV[x].length - 1) == '"') {
-				if ((tl = thisCSV[x].replace(/^\s+"/, '"')).length > 1 && tl.charAt(0) == '"') {
-					thisCSV[x] = thisCSV[x].replace(/^\s*"|"\s*$/g, '').replace(/""/g, '"');
-				} else if (x) {
-					thisCSV.splice(x - 1, 2, [thisCSV[x - 1], thisCSV[x]].join(sep));
-				} else thisCSV = thisCSV.shift().split(sep).concat(thisCSV);
-			} else thisCSV[x].replace(/""/g, '"');
-		} return thisCSV;
-	};
-
-	$.fn.CSVToTable = function(csvFile, options) {
-		var defaults = {
-			tableClass: "CSVTable",
-			loadingDiv: '',
-			theadClass: "",
-			thClass: "",
-			tbodyClass: "",
-			trClass: "",
-			tdClass: "",
-			separator: ",",
-			startLine: 0
-		};
-		var options = $.extend(defaults, options);
-		return this.each(function() {
-			var obj = $(this);
-			var error = '';
-			$(options.loadingDiv).show();
-			data = csvFile;
-			//$.get(csvFile, function(data) {
-			var tableHTML = '<table id="' + $(this).attr('id') + '_table" class="' + options.tableClass + '">';
-			var lines = data.replace('\r','').split('\n');
-			var printedLines = 0;
-			var headerCount = 0;
-			var headers = new Array();
-			$.each(lines, function(lineCount, line) {
-				if ((lineCount == 0) && (typeof(options.headers) != 'undefined')) {
-					headers = options.headers;
-					headerCount = headers.length;
-					tableHTML += '<thead class="' + options.theadClass + '"><tr class="' + options.trClass + '">';
-					$.each(headers, function(headerCount, header) {
-						tableHTML += '<th id="' + header[1] + '" data-override="' + header[1] + '" class="' + options.thClass + '">' + header[0] + '</th>';
-					});
-					tableHTML += '</tr></thead><tbody class="' + options.tbodyClass + '">';
-				}
-				if ((lineCount == options.startLine) && (typeof(options.headers) == 'undefined')) {
-					headers = line.splitCSV(options.separator);
-					headerCount = headers.length;
-					tableHTML += '<thead class="' + options.theadClass + '"><tr class="' + options.trClass + '">';
-					$.each(headers, function(headerCount, header) {
-						tableHTML += '<th class="' + options.thClass + '">' + header + '</th>';
-					});
-					tableHTML += '</tr></thead><tbody class="' + options.tbodyClass + '">';
-				} else if (lineCount >= options.startLine) {
-					var items = line.splitCSV(options.separator);
-					if (items.length > 1) {
-						printedLines++;
-						if (items.length != headerCount) {
-							error += 'error on line ' + lineCount + ': Item count (' + items.length + ') does not match header count (' + headerCount + ') \n';
-						}
-						(printedLines % 2) ? oddOrEven = 'odd' : oddOrEven = 'even';
-						tableHTML += '<tr class="' + options.trClass + ' ' + oddOrEven + '">';
-						$.each(items, function(itemCount, item) {
-							tableHTML += '<td class="' + options.tdClass + '">' + item + '</td>';
-						});
-						tableHTML += '</tr>';
-					}
-				}
-			});
-			tableHTML += '</tbody></table>';
-			$(options.loadingDiv).hide();
-			if (error) {
-				obj.html(error);
-			} else {
-				obj.html(tableHTML);
-				setTimeout(function() {
-					obj.trigger("loadComplete");
-				},0);
-			}
-		});
-	};
-
+  $.fn.CSVToTable = function (csvFile, options) {
+    var defaults = {
+      tableClass: "CSVTable",
+      loadingDiv: "",
+      theadClass: "",
+      thClass: "",
+      tbodyClass: "",
+      trClass: "",
+      tdClass: "",
+      separator: ",",
+      startLine: 0,
+    };
+    var options = $.extend(defaults, options);
+    return this.each(function () {
+      var obj = $(this);
+      var error = "";
+      $(options.loadingDiv).show();
+      data = csvFile;
+      //$.get(csvFile, function(data) {
+      var tableHTML =
+        '<table id="' +
+        $(this).attr("id") +
+        '_table" class="' +
+        options.tableClass +
+        '">';
+      var lines = data.replace("\r", "").split("\n");
+      var printedLines = 0;
+      var headerCount = 0;
+      var headers = new Array();
+      $.each(lines, function (lineCount, line) {
+        if (lineCount == 0 && typeof options.headers != "undefined") {
+          headers = options.headers;
+          headerCount = headers.length;
+          tableHTML +=
+            '<thead class="' +
+            options.theadClass +
+            '"><tr class="' +
+            options.trClass +
+            '">';
+          $.each(headers, function (headerCount, header) {
+            tableHTML +=
+              '<th id="' +
+              header[1] +
+              '" data-override="' +
+              header[1] +
+              '" class="' +
+              options.thClass +
+              '">' +
+              header[0] +
+              "</th>";
+          });
+          tableHTML +=
+            '</tr></thead><tbody class="' + options.tbodyClass + '">';
+        }
+        if (
+          lineCount == options.startLine &&
+          typeof options.headers == "undefined"
+        ) {
+          headers = line.splitCSV(options.separator);
+          headerCount = headers.length;
+          tableHTML +=
+            '<thead class="' +
+            options.theadClass +
+            '"><tr class="' +
+            options.trClass +
+            '">';
+          $.each(headers, function (headerCount, header) {
+            tableHTML +=
+              '<th class="' + options.thClass + '">' + header + "</th>";
+          });
+          tableHTML +=
+            '</tr></thead><tbody class="' + options.tbodyClass + '">';
+        } else if (lineCount >= options.startLine) {
+          var items = line.splitCSV(options.separator);
+          if (items.length > 1) {
+            printedLines++;
+            if (items.length != headerCount) {
+              error +=
+                "error on line " +
+                lineCount +
+                ": Item count (" +
+                items.length +
+                ") does not match header count (" +
+                headerCount +
+                ") \n";
+            }
+            printedLines % 2 ? (oddOrEven = "odd") : (oddOrEven = "even");
+            tableHTML +=
+              '<tr class="' + options.trClass + " " + oddOrEven + '">';
+            $.each(items, function (itemCount, item) {
+              tableHTML +=
+                '<td class="' + options.tdClass + '">' + item + "</td>";
+            });
+            tableHTML += "</tr>";
+          }
+        }
+      });
+      tableHTML += "</tbody></table>";
+      $(options.loadingDiv).hide();
+      if (error) {
+        obj.html(error);
+      } else {
+        obj.html(tableHTML);
+        setTimeout(function () {
+          obj.trigger("loadComplete");
+        }, 0);
+      }
+    });
+  };
 })(jQuery);
--- edit-backend/public/oldAssets/js/libs/jquery.hotkeys.js
+++ edit-backend/public/oldAssets/js/libs/jquery.hotkeys.js
@@ -18,8 +18,7 @@
  * Might be useful, when you want to pass some other data to your handler
  */
 
-(function(jQuery) {
-
+(function (jQuery) {
   jQuery.hotkeys = {
     version: "0.2.0",
 
@@ -87,35 +86,49 @@
       219: "[",
       220: "\\",
       221: "]",
-      222: "'"
+      222: "'",
     },
 
     shiftNums: {
       "`": "~",
-      "1": "!",
-      "2": "@",
-      "3": "#",
-      "4": "$",
-      "5": "%",
-      "6": "^",
-      "7": "&",
-      "8": "*",
-      "9": "(",
-      "0": ")",
+      1: "!",
+      2: "@",
+      3: "#",
+      4: "$",
+      5: "%",
+      6: "^",
+      7: "&",
+      8: "*",
+      9: "(",
+      0: ")",
       "-": "_",
       "=": "+",
       ";": ": ",
-      "'": "\"",
+      "'": '"',
       ",": "<",
       ".": ">",
       "/": "?",
-      "\\": "|"
+      "\\": "|",
     },
 
     // excludes: button, checkbox, file, hidden, image, password, radio, reset, search, submit, url
     textAcceptingInputTypes: [
-      "text", "password", "number", "email", "url", "range", "date", "month", "week", "time", "datetime",
-      "datetime-local", "search", "color", "tel"],
+      "text",
+      "password",
+      "number",
+      "email",
+      "url",
+      "range",
+      "date",
+      "month",
+      "week",
+      "time",
+      "datetime",
+      "datetime-local",
+      "search",
+      "color",
+      "tel",
+    ],
 
     // default input types not to bind to unless bound directly
     textInputTypes: /textarea|input|select/i,
@@ -123,45 +136,55 @@
     options: {
       filterInputAcceptingElements: true,
       filterTextInputs: true,
-      filterContentEditable: true
-    }
+      filterContentEditable: true,
+    },
   };
 
   function keyHandler(handleObj) {
     if (typeof handleObj.data === "string") {
       handleObj.data = {
-        keys: handleObj.data
+        keys: handleObj.data,
       };
     }
 
     // Only care when a possible input has been specified
-    if (!handleObj.data || !handleObj.data.keys || typeof handleObj.data.keys !== "string") {
+    if (
+      !handleObj.data ||
+      !handleObj.data.keys ||
+      typeof handleObj.data.keys !== "string"
+    ) {
       return;
     }
 
     var origHandler = handleObj.handler,
       keys = handleObj.data.keys.toLowerCase().split(" ");
 
-    handleObj.handler = function(event) {
+    handleObj.handler = function (event) {
       //      Don't fire in text-accepting inputs that we didn't directly bind to
-      if (this !== event.target &&
-        (jQuery.hotkeys.options.filterInputAcceptingElements &&
-          jQuery.hotkeys.textInputTypes.test(event.target.nodeName) ||
-          (jQuery.hotkeys.options.filterContentEditable && jQuery(event.target).attr('contenteditable')) ||
+      if (
+        this !== event.target &&
+        ((jQuery.hotkeys.options.filterInputAcceptingElements &&
+          jQuery.hotkeys.textInputTypes.test(event.target.nodeName)) ||
+          (jQuery.hotkeys.options.filterContentEditable &&
+            jQuery(event.target).attr("contenteditable")) ||
           (jQuery.hotkeys.options.filterTextInputs &&
-            jQuery.inArray(event.target.type, jQuery.hotkeys.textAcceptingInputTypes) > -1))) {
+            jQuery.inArray(
+              event.target.type,
+              jQuery.hotkeys.textAcceptingInputTypes,
+            ) > -1))
+      ) {
         return;
       }
 
-      var special = event.type !== "keypress" && jQuery.hotkeys.specialKeys[event.which],
+      var special =
+          event.type !== "keypress" && jQuery.hotkeys.specialKeys[event.which],
         character = String.fromCharCode(event.which).toLowerCase(),
         modif = "",
         possible = {};
 
-      jQuery.each(["alt", "ctrl", "shift"], function(index, specialKey) {
-
-        if (event[specialKey + 'Key'] && special !== specialKey) {
-          modif += specialKey + '+';
+      jQuery.each(["alt", "ctrl", "shift"], function (index, specialKey) {
+        if (event[specialKey + "Key"] && special !== specialKey) {
+          modif += specialKey + "+";
         }
       });
 
@@ -170,14 +193,17 @@
         modif += "meta+";
       }
 
-      if (event.metaKey && special !== "meta" && modif.indexOf("alt+ctrl+shift+") > -1) {
+      if (
+        event.metaKey &&
+        special !== "meta" &&
+        modif.indexOf("alt+ctrl+shift+") > -1
+      ) {
         modif = modif.replace("alt+ctrl+shift+", "hyper+");
       }
 
       if (special) {
         possible[modif + special] = true;
-      }
-      else {
+      } else {
         possible[modif + character] = true;
         possible[modif + jQuery.hotkeys.shiftNums[character]] = true;
 
@@ -195,10 +221,9 @@
     };
   }
 
-  jQuery.each(["keydown", "keyup", "keypress"], function() {
+  jQuery.each(["keydown", "keyup", "keypress"], function () {
     jQuery.event.special[this] = {
-      add: keyHandler
+      add: keyHandler,
     };
   });
-
 })(jQuery || this.jQuery || window.jQuery);
