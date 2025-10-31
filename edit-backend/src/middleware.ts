e43b7d646fa2b18d6fb532f46c672b957158cfa4
Liam Burnand <liam.burnand@ystv.co.uk>
2025-10-31 23:28:07 +0000

    Moved to the end

--- edit-backend/src/middleware.ts
+++ edit-backend/src/middleware.ts
@@ -77,6 +77,6 @@ export default withAuth(
 export const config = {
   matcher: [
     // Protect all routes except the sign-in and auth routes
-    "/((?!api/auth|api/s3url|api/frontend|oldAssets|auth/signin|_next|favicon.ico|images|public).*)",
+    "/((?!api/auth|api/s3url|api/frontend|auth/signin|_next|favicon.ico|images|public|oldAssets).*)",
   ],
 };
