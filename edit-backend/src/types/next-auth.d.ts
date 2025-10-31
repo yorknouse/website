146bf08ec57f4f0ff1a1689ac253d5ab7a47bca2
Liam Burnand <liam.burnand@ystv.co.uk>
2025-10-31 21:06:42 +0000

    Adding an actions map

--- edit-backend/src/types/next-auth.d.ts
+++ edit-backend/src/types/next-auth.d.ts
@@ -6,6 +6,7 @@ declare module "next-auth" {
       internalId?: number;
       internalToken?: string;
       positions?: (string | null)[];
+      actions?: Map<number, boolean>;
     } & DefaultSession["user"];
   }
 
@@ -13,6 +14,7 @@ declare module "next-auth" {
     internalId?: number;
     internalToken?: string;
     positions?: (string | null)[];
+    actions?: Map<number, boolean>;
   }
 }
 
@@ -21,5 +23,6 @@ declare module "next-auth/jwt" {
     internalId?: number;
     internalToken?: string;
     positions?: (string | null)[];
+    actions?: Map<number, boolean>;
   }
 }
