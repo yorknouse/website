aed629405a9d6b5a69490ccd5bfd1b3a57ddd149
Liam Burnand <liam.burnand@ystv.co.uk>
2025-11-01 09:32:45 +0000

    Interesting import fix

--- html/admin/common/headSecure.php
+++ html/admin/common/headSecure.php
@@ -1,7 +1,7 @@
 <?php
 global $CONFIG;
 require_once __DIR__ . '/head.php';
-require_once __DIR__ . '/../common/authLib.php';
+require_once __DIR__ . '/authLib.php';
 
 if (!$GLOBALS['AUTH']->login) {
     $_SESSION['return'] = "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
