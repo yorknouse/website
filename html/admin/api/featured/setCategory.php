3aad786a35a69cc463da58d18bb722087820e79e
Liam Burnand <liam.burnand@ystv.co.uk>
2025-11-05 00:02:54 +0000

    Making it error if there is a problem

--- html/admin/api/featured/setCategory.php
+++ html/admin/api/featured/setCategory.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../apiHeadSecure.php';
 if (!isset($_POST['categoryid']) or !is_numeric($_POST['categoryid']) or !isset($_POST['selection']) or !$AUTH->permissionCheck(21)) finish(false, ["code" => "PARAM", "message"=> "Parameter error"]);
 
 $DBLIB->where("categories_id", $bCMS->sanitizeString($_POST['categoryid']));
-if ($DBLIB->update("categories", ["categories_featured" => $bCMS->sanitizeString($_POST['selection'])]))
+if (!$DBLIB->update("categories", ["categories_featured" => $bCMS->sanitizeString($_POST['selection'])]))
     finish(false, ["code" => "PARAM", "message"=> "Update error"]);
 
 $bCMS->auditLog("EDIT FEATURED", "categories", $bCMS->sanitizeString($_POST['selection']), $AUTH->data['users_userid']);
