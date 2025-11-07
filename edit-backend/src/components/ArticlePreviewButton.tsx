2f0120c26c9a7bde2222f805ffd67d6deab40216
Liam Burnand <liam.burnand@ystv.co.uk>
2025-11-07 22:22:30 +0000

    Adding a preview button

---
+++ edit-backend/src/components/ArticlePreviewButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function ArticlePreviewButton({
+                                                 props: { articleID },
+                                             }: {
+    props: {
+        articleID: number;
+    };
+}) {
+    return (
+<a
+    href={`/api/backend/preview/${articleID}`}
+    target="_blank"
+    className={"px-3 py-1 rounded bg-blue-600 text-white btn btn-sm btn-default cursor-pointer"}
+>
+    Preview on Site
+</a>
+);
+}
