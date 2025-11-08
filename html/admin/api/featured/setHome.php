c0760749675949b7496847338574ec4bd8e3aeb5
Liam Burnand <liam.burnand@ystv.co.uk>
2025-11-08 22:10:42 +0000

    Inverting and removing implode explode

--- html/admin/api/featured/setHome.php
+++ html/admin/api/featured/setHome.php
@@ -4,10 +4,10 @@ require_once __DIR__ . '/../apiHeadSecure.php';
 
 if (!isset($_POST['selection']) or !$AUTH->permissionCheck(22)) finish(false, ["code" => "PARAM", "message"=> "Parameter error"]);
 
-if ($DBLIB->insert("featuredHome", ["featuredHome_articles" => implode(",", explode(",", $bCMS->sanitiseString($_POST['selection']))), "featuredHome_timestamp" => date('Y-m-d H:i:s'), "users_userid" => $AUTH->data['users_userid']]))
+if (!$DBLIB->insert("featuredHome", ["featuredHome_articles" => $bCMS->sanitiseString($_POST['selection']), "featuredHome_timestamp" => date('Y-m-d H:i:s'), "users_userid" => $AUTH->data['users_userid']]))
     finish(false, ["code" => "PARAM", "message"=> "Update error"]);
 
-$bCMS->auditLog("SET FEATURED", "featuredHome", implode(",", explode(",", $bCMS->sanitiseString($_POST['selection']))), $AUTH->data['users_userid']);
+$bCMS->auditLog("SET FEATURED", "featuredHome", $bCMS->sanitiseString($_POST['selection']), $AUTH->data['users_userid']);
 $bCMS->cacheClear($CONFIG['ROOTFRONTENDURL']); //Homepage has a list on it
 
 finish(true);
