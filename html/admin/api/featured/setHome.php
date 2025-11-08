d7d030532a5cb255d60a413261aa03c945d69f82
Ben <benharris@manx.net>
2025-11-08 18:52:27 +0000

    Fix for broken insert syntax in setHome features endpoint

--- html/admin/api/featured/setHome.php
+++ html/admin/api/featured/setHome.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../apiHeadSecure.php';
 
 if (!isset($_POST['selection']) or !$AUTH->permissionCheck(22)) finish(false, ["code" => "PARAM", "message"=> "Parameter error"]);
 
-if ($DBLIB->insert("featuredHome", ["featuredHome_articles" < implode(",", explode(",", $bCMS->sanitiseString($_POST['selection']))), "featuredHome_timestamp" => date('Y-m-d H:i:s'), "users_userid" => $AUTH->data['users_userid']]))
+if ($DBLIB->insert("featuredHome", ["featuredHome_articles" => implode(",", explode(",", $bCMS->sanitiseString($_POST['selection']))), "featuredHome_timestamp" => date('Y-m-d H:i:s'), "users_userid" => $AUTH->data['users_userid']]))
     finish(false, ["code" => "PARAM", "message"=> "Update error"]);
 
 $bCMS->auditLog("SET FEATURED", "featuredHome", implode(",", explode(",", $bCMS->sanitiseString($_POST['selection']))), $AUTH->data['users_userid']);
