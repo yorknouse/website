6c4efab9effeee49e3793e9ea63f4000ac3bef6e
Liam Burnand <liam.burnand@ystv.co.uk>
2025-11-14 10:35:24 +0000

    Adding lang

--- html/common/libs/email/main.php
+++ html/common/libs/email/main.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/../../config.php';
 function outputEmail(string $html): string {
     global $CONFIG;
     return '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-	<html xmlns="http://www.w3.org/1999/xhtml">
+	<html xmlns="http://www.w3.org/1999/xhtml" lang="en-GB">
 	<head>
 		<title>Message from ' . $CONFIG->PROJECT_NAME . '</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
