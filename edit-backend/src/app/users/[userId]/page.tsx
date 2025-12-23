40d9bf6ac63f7c71a0c07c34c8cd848dcd642f04
Liam Burnand <liam.burnand@ystv.co.uk>
2025-12-23 10:48:32 +0000

    Moved the page to a client prop

--- edit-backend/src/app/users/[userId]/page.tsx
+++ edit-backend/src/app/users/[userId]/page.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import { checkUserPermissions, GetUserData } from "@/lib/auth";
 import { z } from "zod";
 import prisma from "@/lib/prisma";
-import Image from "next/image";
 import { s3URL } from "@/lib/s3URL";
-import AvatarUploader from "@/components/AvatarUploader";
+import UserPage from "@/components/UserPage";
 
 export const metadata: Metadata = {
   title: "User",
@@ -46,38 +45,7 @@ export default async function User({
     thumbnailURL = await s3URL(Number(user.users_thumbnail));
   }
 
-  return (
-    <div>
-      <p>{user.users_userid}</p>
-      <p>{user.users_name1}</p>
-      <p>{user.users_name2}</p>
-      <p>{user.users_pronouns}</p>
-      <p>{user.users_googleAppsUsernameYork}</p>
-      <p>{user.users_googleAppsUsernameNouse}</p>
-      <p>{user.users_bio}</p>
-      <a
-        href={`https://nouse.co.uk/author/${user.users_userid}`}
-        target={"_blank"}
-      >
-        https://nouse.co.uk/author/{user.users_userid}
-      </a>
-      {thumbnailURL && (
-        <Image
-          src={thumbnailURL}
-          alt={"User thumbnail"}
-          width={400}
-          height={400}
-        />
-      )}
-      <p>{user.users_social_facebook}</p>
-      <p>{user.users_social_twitter}</p>
-      <p>{user.users_social_instagram}</p>
-      <p>{user.users_social_linkedin}</p>
-      <p>{user.users_social_snapchat}</p>
-      <AvatarUploader
-        userId={user.users_userid}
-        backendUrl={"http://localhost:419"}
-      />
-    </div>
+    return (
+    <UserPage props={{user, initialThumbnailURL: thumbnailURL}} />
   );
 }
