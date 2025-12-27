f33778f5aa1753d99af7dec6f3286182c65c2f39
Liam Burnand <liam.burnand@nouse.co.uk>
2025-12-27 21:10:08 +0000

    Updating packages (#143)
    * Updating packages and prisma
    
    * Updating readers-frontend
    
    * Removing old files from readers frontend
    
    * Moved from readers frontend to edit backend
    
    * Prettified
    
    * Updated

--- edit-backend/src/lib/prisma.ts
+++ edit-backend/src/lib/prisma.ts
@@ -1,8 +1,24 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const adapter = new PrismaMariaDb({
+  host: process.env.MYSQL_HOSTNAME,
+  port: Number(process.env.MYSQL_PORT),
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  connectionLimit: 10,
+});
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
--- readers-frontend/build-utils/baseUrl.ts
+++
@@ -1,3 +0,0 @@
-const baseUrl = "https://nouse.co.uk/";
-
-export default baseUrl;
