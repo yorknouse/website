8e1827001bd58b0527ee32da373abe3f4fc9f8ef
Liam Burnand <liam.burnand@ystv.co.uk>
2025-12-27 02:20:38 +0000

    Improving the list users view

--- edit-backend/src/app/users/page.tsx
+++ edit-backend/src/app/users/page.tsx
@@ -2,11 +2,12 @@ import type { Metadata } from "next";
 import { checkUserPermissions, GetUserData } from "@/lib/auth";
 import { sanitiseSearchTerm } from "@/lib/validation/searchTerms";
 import SearchBar from "@/components/SearchBar";
-import { ChartLine, Check, EyeIcon, PenLine, Trash } from "lucide-react";
-import dateFormatter from "@/lib/dateFormatter";
-import ArticleDeleteButton from "@/components/ArticleDeleteButton";
+import { Hand, Hourglass, PenLine, Trash } from "lucide-react";
+import UserDeleteButton from "@/components/UserDeleteButton";
 import Pagination from "@/components/Pagination";
 import { getUsers } from "@/lib/users";
+import UserSuspendUnsuspendButton from "@/components/UserSuspendUnsuspendButton";
+import UserEndAllPositionsButton from "@/components/UserEndAllPositionsButton";
 
 export const metadata: Metadata = {
   title: "Users",
@@ -41,6 +42,11 @@ export default async function Users({
 
   let i = -1;
 
+  const canEdit = checkUserPermissions(5, userData.actions);
+  const canSuspend = checkUserPermissions(9, userData.actions);
+  const canEndAllRolesNow = checkUserPermissions(13, userData.actions);
+  const canDelete = checkUserPermissions(15, userData.actions);
+
   return (
     <div className="lg:flex min-h-screen bg-gray-200 text-gray-900">
       <div className={"flex flex-col"}>
@@ -54,14 +60,14 @@ export default async function Users({
         <table className={"table-auto bg-white"}>
           <thead className={"border-b-4 border-gray-600"}>
             <tr>
-              <th>ID</th>
-              <th>First name</th>
-              <th>Last name</th>
-              <th>York username</th>
-              <th>Nouse username</th>
-              <th>Roles No.</th>
-              {/*<th>Published author(s)</th>*/}
-              {/*<th>Actions</th>*/}
+              <th className={"px-4 py-2"}>ID</th>
+              <th className={"px-4 py-2"}>First name</th>
+              <th className={"px-4 py-2"}>Last name</th>
+              <th className={"px-4 py-2"}>York username</th>
+              <th className={"px-4 py-2"}>Nouse username</th>
+              <th className={"px-4 py-2"}>Roles</th>
+              <th className={"px-4 py-2"}>Labels</th>
+              <th className={"px-4 py-2"}>Actions</th>
             </tr>
           </thead>
           <tbody>
@@ -70,17 +76,8 @@ export default async function Users({
               return (
                 <tr
                   key={user.users_userid}
-                  //             className={`
-                  // ${
-                  //                 !article.articles_showInLists &&
-                  //                 !article.articles_showInSearch
-                  //                     ? "bg-red-300"
-                  //                     : !article.articles_showInLists
-                  //                         ? "bg-amber-300"
-                  //                         : i % 2 != 0
-                  //                             ? "bg-gray-100"
-                  //                             : ""
-                  //             }`}
+                  className={`
+                  ${user.users_suspended && i % 2 != 0 ? "bg-red-300" : user.users_suspended ? "bg-red-200" : (!user.userPositions || user.userPositions.length < 1) && i % 2 != 0 ? "bg-amber-100" : !user.userPositions || user.userPositions.length < 1 ? "bg-amber-50" : i % 2 != 0 ? "bg-gray-100" : ""}`}
                 >
                   <th className={"px-4 py-2"}>{user.users_userid}</th>
                   <td className={"px-4 py-2"}>{user.users_name1}</td>
@@ -91,151 +88,115 @@ export default async function Users({
                   <td className={"px-4 py-2"}>
                     {user.users_googleAppsUsernameNouse}
                   </td>
-                  <td className={"px-4 py-2"}>{user.userPositions.length}</td>
-                  {/*        <td className={"px-4 py-2"}>*/}
-                  {/*            {article.articlesDrafts[0].articlesDrafts_headline}*/}
-                  {/*            {!article.articles_showInLists &&*/}
-                  {/*            !article.articles_showInSearch ? (*/}
-                  {/*                <span className="inline-block text-xs font-medium rounded-full px-2 py-0.5 mb-1 mr-1 shadow-sm bg-red-500 text-white ml-2">*/}
-                  {/*  Draft*/}
-                  {/*</span>*/}
-                  {/*            ) : !article.articles_showInLists ? (*/}
-                  {/*                <span className="inline-block text-xs font-medium rounded-full px-2 py-0.5 mb-1 mr-1 shadow-sm bg-amber-500 text-white ml-2">*/}
-                  {/*  Show in search only*/}
-                  {/*</span>*/}
-                  {/*            ) : (*/}
-                  {/*                <></>*/}
-                  {/*            )}*/}
-                  {/*        </td>*/}
-                  {/*        <td className={"px-4 py-2"}>*/}
-                  {/*            {article.articles_categories.map((category) => (*/}
-                  {/*                <span*/}
-                  {/*                    key={category.categories_id}*/}
-                  {/*                    className="inline-block text-xs font-medium rounded-full px-2 py-0.5 mb-1 mr-1 shadow-sm"*/}
-                  {/*                    style={{*/}
-                  {/*                        backgroundColor:*/}
-                  {/*                            category.category.categories_backgroundColor ||*/}
-                  {/*                            "#3B82F6", // default blue if none*/}
-                  {/*                        color: category.category*/}
-                  {/*                            .categories_backgroundColorContrast*/}
-                  {/*                            ? `#${category.category.categories_backgroundColorContrast}`*/}
-                  {/*                            : "#fff",*/}
-                  {/*                    }}*/}
-                  {/*                >*/}
-                  {/*  {category.category.categories_displayName}*/}
-                  {/*</span>*/}
-                  {/*            ))}*/}
-                  {/*        </td>*/}
-                  {/*        <td className={"px-4 py-2"}>*/}
-                  {/*            {article.edition && (*/}
-                  {/*                <a href={`/editions/${article.editions_id}`}>*/}
-                  {/*                    {article.edition.editions_printNumber !== null ? (*/}
-                  {/*                        <>№{article.edition.editions_printNumber}</>*/}
-                  {/*                    ) : (*/}
-                  {/*                        <Check />*/}
-                  {/*                    )}*/}
-                  {/*                </a>*/}
-                  {/*            )}*/}
-                  {/*        </td>*/}
-                  {/*        <td className={"px-4 py-2"}>*/}
-                  {/*            {new Intl.DateTimeFormat("en-GB", {*/}
-                  {/*                weekday: "short",*/}
-                  {/*                day: "2-digit",*/}
-                  {/*                month: "short",*/}
-                  {/*                year: "numeric",*/}
-                  {/*            }).format(article.articles_published!)}*/}
-                  {/*        </td>*/}
-                  {/*        <td className={"px-4 py-2"}>*/}
-                  {/*            {new Intl.DateTimeFormat("en-GB", {*/}
-                  {/*                weekday: "short",*/}
-                  {/*                day: "2-digit",*/}
-                  {/*                month: "short",*/}
-                  {/*                year: "numeric",*/}
-                  {/*                hour: "2-digit",*/}
-                  {/*                minute: "2-digit",*/}
-                  {/*                second: "2-digit",*/}
-                  {/*            }).format(article.articles_updated!)}*/}
-                  {/*        </td>*/}
-                  {/*        <td className={"px-4 py-2"}>*/}
-                  {/*            {article.users.map((author) => (*/}
-                  {/*                <p key={author.users.users_userid}>*/}
-                  {/*                    <a href={`/articles?user=${author.users.users_userid}`}>*/}
-                  {/*                        {author.users.users_name1} {author.users.users_name2}*/}
-                  {/*                    </a>*/}
-                  {/*                </p>*/}
-                  {/*            ))}*/}
-                  {/*        </td>*/}
-                  {/*        <td className={"px-2 py-2"}>*/}
-                  {/*            {article.articles_showInSearch &&*/}
-                  {/*                article.articles_published &&*/}
-                  {/*                article.articles_published < new Date() && (*/}
-                  {/*                    <>*/}
-                  {/*    <span className={"px-2 py-2"}>*/}
-                  {/*      <a*/}
-                  {/*          href={`${process.env.FRONTEND_URL}articles/${dateFormatter*/}
-                  {/*              .format(*/}
-                  {/*                  article.articles_published || new Date(0),*/}
-                  {/*              ) // split -> reverse -> join = DD/MM/YYYY -> YYYY/MM/DD*/}
-                  {/*              .split("/")*/}
-                  {/*              .reverse()*/}
-                  {/*              .join("/")}/${String(article.articles_slug)}`}*/}
-                  {/*          target={"_blank"}*/}
-                  {/*          className={*/}
-                  {/*              "inline-block rounded-sm px-2 py-2 shadow-sm bg-gray-200 hover:bg-gray-300 transition"*/}
-                  {/*          }*/}
-                  {/*          title={"View on website"}*/}
-                  {/*      >*/}
-                  {/*        <EyeIcon />*/}
-                  {/*      </a>*/}
-                  {/*    </span>*/}
-                  {/*                        <span className={"px-2 py-2"}>*/}
-                  {/*      <a*/}
-                  {/*          href={`${process.env.GRAFANA_URL}d/article/article-stats?var-articleid=${article.articles_id}&from=${(article.articles_published.valueOf() - 7200) * 1000}&to=now`}*/}
-                  {/*          target={"_blank"}*/}
-                  {/*          className={*/}
-                  {/*              "inline-block rounded-sm px-2 py-2 shadow-sm bg-gray-200 hover:bg-gray-300 transition"*/}
-                  {/*          }*/}
-                  {/*          title={"View article stats"}*/}
-                  {/*      >*/}
-                  {/*        <ChartLine />*/}
-                  {/*      </a>*/}
-                  {/*    </span>*/}
-                  {/*                    </>*/}
-                  {/*                )}*/}
-                  {/*            {canEdit && (*/}
-                  {/*                <span className={"px-2 py-2"}>*/}
-                  {/*  <a*/}
-                  {/*      href={`/articles/${article.articles_id}`}*/}
-                  {/*      className={*/}
-                  {/*          "inline-block rounded-sm px-2 py-2 shadow-sm bg-gray-200 hover:bg-gray-300 transition"*/}
-                  {/*      }*/}
-                  {/*      title={"Edit article"}*/}
-                  {/*  >*/}
-                  {/*    <PenLine />*/}
-                  {/*  </a>*/}
-                  {/*</span>*/}
-                  {/*            )}*/}
-                  {/*            {canDelete && (*/}
-                  {/*                <ArticleDeleteButton*/}
-                  {/*                    props={{*/}
-                  {/*                        articleID: article.articles_id,*/}
-                  {/*                        headline:*/}
-                  {/*                        article.articlesDrafts[0].articlesDrafts_headline,*/}
-                  {/*                        userID: Number(userData.id),*/}
-                  {/*                        userActions: userData.actions,*/}
-                  {/*                    }}*/}
-                  {/*                >*/}
-                  {/*  <span className={"px-2 py-2"}>*/}
-                  {/*    <a*/}
-                  {/*        className="inline-block rounded-sm px-2 py-2 shadow-sm bg-red-500 text-white hover:bg-red-600 transition"*/}
-                  {/*        title="Delete article"*/}
-                  {/*    >*/}
-                  {/*      <Trash />*/}
-                  {/*    </a>*/}
-                  {/*  </span>*/}
-                  {/*                </ArticleDeleteButton>*/}
-                  {/*            )}*/}
-                  {/*        </td>*/}
+                  <td className={"px-4 py-2"}>
+                    {user.userPositions &&
+                      user.userPositions.map((userPosition) => (
+                        <div key={userPosition.userPositions_id}>
+                          {userPosition.positions.positions_displayName}
+                        </div>
+                      ))}
+                  </td>
+                  <td className={"px-4 py-2"}>
+                    {user.users_suspended ? (
+                      <span className={"px-1"}>
+                        <span
+                          className={
+                            "inline-block rounded-sm shadow-sm bg-red-400 px-2 py-1 text-xs text-white"
+                          }
+                        >
+                          Suspended
+                        </span>
+                      </span>
+                    ) : (
+                      <></>
+                    )}
+                    {(!user.userPositions || user.userPositions.length < 1) && (
+                      <span className={"px-1"}>
+                        <span
+                          className={
+                            "inline-block rounded-sm shadow-sm bg-orange-300 px-2 py-1 text-xs text-white"
+                          }
+                        >
+                          Cannot log in
+                        </span>
+                      </span>
+                    )}
+                  </td>
+                  <td className={"px-2 py-2"}>
+                    {canEdit && (
+                      <span className={"px-2 py-2"}>
+                        <a
+                          href={`/users/${user.users_userid}`}
+                          className={
+                            "inline-block rounded-sm px-2 py-2 shadow-sm bg-gray-200 hover:bg-gray-300 transition"
+                          }
+                          title={"Edit User"}
+                        >
+                          <PenLine />
+                        </a>
+                      </span>
+                    )}
+                    {canEndAllRolesNow && (
+                      <UserEndAllPositionsButton
+                        props={{
+                          userToID: user.users_userid,
+                          userName: user.users_name1 + " " + user.users_name2,
+                          userID: Number(userData.id),
+                          userActions: userData.actions,
+                        }}
+                      >
+                        <span className={"px-2 py-2"}>
+                          <a
+                            className={`inline-block rounded-sm px-2 py-2 shadow-sm text-white bg-orange-300 hover:bg-orange-400 transition`}
+                            title={`End all Positions now`}
+                          >
+                            <Hourglass />
+                          </a>
+                        </span>
+                      </UserEndAllPositionsButton>
+                    )}
+                    {canSuspend && (
+                      <UserSuspendUnsuspendButton
+                        props={{
+                          suspendUnsuspend: user.users_suspended
+                            ? "unsuspend"
+                            : "suspend",
+                          userToID: user.users_userid,
+                          userName: user.users_name1 + " " + user.users_name2,
+                          userID: Number(userData.id),
+                          userActions: userData.actions,
+                        }}
+                      >
+                        <span className={"px-2 py-2"}>
+                          <a
+                            className={`inline-block rounded-sm px-2 py-2 shadow-sm text-white ${user.users_suspended ? "bg-green-500 hover:bg-green-600" : "bg-red-500 hover:bg-red-600"} transition`}
+                            title={`${user.users_suspended ? "Unsuspend" : "Suspend"} user`}
+                          >
+                            <Hand />
+                          </a>
+                        </span>
+                      </UserSuspendUnsuspendButton>
+                    )}
+                    {canDelete && (
+                      <UserDeleteButton
+                        props={{
+                          userToID: user.users_userid,
+                          userName: user.users_name1 + " " + user.users_name2,
+                          userID: Number(userData.id),
+                          userActions: userData.actions,
+                        }}
+                      >
+                        <span className={"px-2 py-2"}>
+                          <a
+                            className="inline-block rounded-sm px-2 py-2 shadow-sm bg-red-500 text-white hover:bg-red-600 transition"
+                            title="Delete user"
+                          >
+                            <Trash />
+                          </a>
+                        </span>
+                      </UserDeleteButton>
+                    )}
+                  </td>
                 </tr>
               );
             })}
